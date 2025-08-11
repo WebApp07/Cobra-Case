c7927b417b2f684ce1a68410f4c2d56b2dc8493c
webapp07 <aminezouguig@proton.me>
2025-08-11 16:42:05 +0200

    make logo like pro, Add some animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import MaxWithWrapper from "@/components/MaxWithWrapper";
@@ -13,15 +14,12 @@ export default function Home() {
         <MaxWithWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
-              <div>
-                <Image
-                  src="/snake-1.png"
-                  alt="snake-logo"
-                  className="w-full"
-                  width={40}
-                  height={40}
-                />
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
+                <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28" />
+                <img src="/snake-1.png" className="w-full" />
               </div>
+
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your Image on{" "}
                 <span className="bg-green-600 px-2 text-white">Custom</span>
@@ -103,7 +101,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-
           <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
             <div className="relative md:max-w-xl">
               <img
@@ -199,18 +196,18 @@ export default function Home() {
                   yellow-ish color after a couple weeks. Love it.&quot;
                 </p>
               </div>
-            </div>
-            <div className="flex gap-4 mt-2">
-              <img
-                className="rounded-full h-12 w-12 object-cover"
-                src="/users/user-4.jpg"
-                alt="user"
-              />
-              <div className="flex flex-col">
-                <p className="font-semibold"> Jonathan</p>
-                <div className="flex gap-1.5 items-center text-zinc-600">
-                  <Check className="h-4 w-4 stroke-[3px] text-green-600" />
-                  <p className="text-sm">Verified Purchase</p>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold"> Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
                 </div>
               </div>
             </div>
