850b92cc7226cf0478e9ab977b838f4daa4ec2e1
webapp07 <aminezouguig@proton.me>
2025-08-16 00:16:51 +0200

    Implememt footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import MaxWithWrapper from "./MaxWithWrapper";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white h-20 relative">
+      <MaxWithWrapper>
+        <div className="border-t border-gray-200" />
+        <div className="h-full flex flex-col md:flex-row md:justify-between justify-center items-center">
+          <div className="text-center md:text-left pb-2 md:pd-0">
+            <p className="text-sm text-muted-foreground">
+              Copyright &copy;{new Date().getFullYear()} Developed by{" "}
+              <span className="font-bold text-green-600">Amine Zouguig</span>{" "}
+              All rights reserved
+            </p>
+          </div>
+
+          <div className="flex items-center justify-center">
+            <div className="flex space-x-8">
+              <Link
+                href="#"
+                className="text-sm text-muted-foreground hover:text-gray-600"
+              >
+                Terms
+              </Link>
+              <Link
+                href="#"
+                className="text-sm text-muted-foreground hover:text-gray-600"
+              >
+                privacy Policy
+              </Link>
+              <Link
+                href="#"
+                className="text-sm text-muted-foreground hover:text-gray-600"
+              >
+                Cookie Policy
+              </Link>
+            </div>
+          </div>
+        </div>
+      </MaxWithWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
